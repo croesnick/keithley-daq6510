e79425ec9668326440d3ce495081cf2feaa35c6c
Carsten Rösnick-Neugebauer <carsten.roesnick-neugebauer@tngtech.com>
2019-12-28 10:27:23 +0100

    Add docstrings

--- src/keithley_daq6510/scpi/operation/__init__.py
+++ src/keithley_daq6510/scpi/operation/__init__.py
@@ -9,27 +9,70 @@ from ...scpi.formatter.operation import comma_separated
 
 logger = logging.getLogger(__name__)
 
-FormatterT = Callable[..., str]
+FormatterT = Callable[[str, Any], str]
 
 
 class Operation:
-    def __init__(self, command: str, formatter: Optional[FormatterT] = None,
+    def __init__(self, command: str, formatter: FormatterT = comma_separated,
                  parameters: Optional[Iterable[Parameter]] = None):
+        """
+        SCPI operation (command or query) builder.
+
+        Example:
+            Intended to be used in class attribute initialization::
+
+                class Sample:
+                    idn = Operation('*IDN?')
+
+        Args:
+            command: Full SCPI command string (without parameters). SCPI queries are expected to end with a ``?``.
+            formatter: Customize building and formatting the SCPI operation string before sending it to the device.
+                Defaults to format ``<scpi-command> <arg1>[, <arg2>[, ...<argN>]]``.
+            parameters: Define iterable of allowed parameters, including optional validation and formatting.
+        """
+
         self.command = command.strip().upper()
-        self.formatter = formatter if formatter is not None else comma_separated
+        self.formatter = formatter
         self.parameters = parameters
 
-    def __get__(self, caller, _type=None) -> Callable[..., Any]:
+    def __get__(self, caller: MessageBasedConnection, cls=None) -> Callable[..., Any]:
+        """
+        Get function wrapping SCPI operation parameter validation and device communication, and bind it to ``caller``.
+
+        Args:
+            caller: Instrument instance.
+            cls: Instrument instance type.
+
+        Returns:
+            Callable implementing the actual parameter validation and communication with the device.
+        """
+
         def _operation(oself: MessageBasedConnection, *args: Any, **kwargs: Any) -> Any:
+            """
+            Actual parameter validation and operation execution.
+
+            Args:
+                oself: Device connection.
+                *args: SCPI operation arguments.
+                **kwargs: SCPI operation keyword arguments.
+
+            Returns:
+                Whatever is returned by the executed SCPI operation.
+
+            Raises:
+                ValueError: If a value is not allowed for a parameter (usually if not whitelisted).
+                TypeError: If required parameters are missing or custom validator is of unsupported type.
+            """
+
             if self.parameters is None:
-                return self._run(oself, *args, **kwargs)
+                return self._execute(oself, *args, **kwargs)
             else:
                 arguments = []
                 for value, field in zip_longest(args, self.parameters, fillvalue=None):
                     if field is None:
                         break
                     if value is None and not field.optional:
-                        raise ValueError(f'Expected field {field.name}, but no argument were given')
+                        raise TypeError(f'Expected field {field.name}, but no argument were given')
 
                     if field.validator is None:
                         logger.debug(f'No validation requested for {field.name}={value}')
@@ -44,11 +87,23 @@ class Operation:
                     logger.debug(f'Validated {field.name}={value}')
                     arguments.append(field.formatter(value))
 
-                return self._run(oself, *arguments)
+                return self._execute(oself, *arguments)
 
         return types.MethodType(_operation, caller)
 
-    def _run(self, connection: MessageBasedConnection, *args, **kwargs) -> Any:
+    def _execute(self, connection: MessageBasedConnection, *args, **kwargs) -> Any:
+        """
+        Build SCPI operation string and send it over the provided ``connection``.
+
+        Args:
+            connection: Device connection.
+            *args: SCPI operation arguments.
+            **kwargs: SCPI operation keyword arguments.
+
+        Returns:
+            Whatever is returned by the executed SCPI operation.
+        """
+
         send_message_fn = connection.query if self.command.endswith('?') else connection.command
 
         message = self.formatter(self.command, *args, **kwargs)  # type: ignore
--- src/keithley_daq6510/scpi/operation/parameter.py
+++ src/keithley_daq6510/scpi/operation/parameter.py
@@ -7,6 +7,33 @@ Validator = Union[Iterable[T], Callable[[T], bool]]
 
 class Parameter(Generic[T]):
     def __init__(self, name: str, formatter: Callable[[T], str] = str, validator: Optional[Validator] = None):
+        """
+        SCPI operation parameter abstraction.
+
+        Examples:
+            Most simple parameter with just a name::
+
+                Parameter('bit_number')
+
+            Parameter with range validation::
+
+                Parameter('bit_number', validator=range(0, 15))
+
+            Custom validator function rather than iterable of allowed values::
+
+                Parameter('bit_number', validator=lambda n: 0 <= n <= 14)
+
+            Parameter with custom formatter::
+
+                Parameter('bit_number', formatter=lambda n: f'#b{n:b}')
+
+        Args:
+            name: Parameter name, preferably as in the official device documentation.
+            formatter: Custom parameter-to-string converter to be called within SCPI operation string construction.
+            validator: Custom parameter whitelist or validation function.
+                Accepts an iterable of allowed values (whitelist) or a function.
+                If given a function, it is expected to return True if and only if the passed value is allowed.
+        """
         self.name = name
         self.formatter = formatter
         self.validator = validator
